d17d1b5afe0270720995822ace489770f8b09278
SeptianTito123 <SeptianTito123>
2023-10-12 14:03:27 +0700

    commit

--- ForKelipatan26.java
+++ ForKelipatan26.java
@@ -4,6 +4,7 @@ public class ForKelipatan26 {
     public static void main(String[] args) {
         Scanner scan26 =new Scanner (System.in);
         int kelipatan, jumlah, counter;
+        double rataRata;
         jumlah = 0;
         counter = 0;
         
@@ -17,8 +18,13 @@ public class ForKelipatan26 {
 
                 
             }
-        }       System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-                System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        }   
+        if (counter > 0) {
+            rataRata = (double) jumlah / counter;
 
+                System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+                System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+                System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
+        }
     }
 }

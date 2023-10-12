4d54349299d3919c56447ff2d58584762bbaeb11
SeptianTito123 <SeptianTito123>
2023-10-12 13:23:19 +0700

    commit

--- DoWhileCuti26.java
+++ DoWhileCuti26.java
@@ -3,5 +3,29 @@ public class DoWhileCuti26 {
 
     public static void main(String[] args) {
         Scanner scan26 =new Scanner (System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = scan26.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = scan26.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("jumlah hari: ");
+                jumlahHari = scan26.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
     }
 }
